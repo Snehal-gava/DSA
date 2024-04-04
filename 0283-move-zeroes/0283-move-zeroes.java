cb9a777412c08ee7ab2d6790b366cc93a3354764
Snehal Gavande <75498566+Snehal-gava@users.noreply.github.com>
2024-04-04 22:38:05 +0530

    Time: 1 ms (100.00%), Space: 45.5 MB (92.86%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,22 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int c=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
-                c++;
-            }
-        }
-        int len = nums.length;
-        int j=0,k=0;
-        while(k<len){
-            if(nums[k]!=0){
-                nums[j++]=nums[k];
-            }
-            k++;
-        }
-        while(j<len){
-            nums[j++]=0;
-        }
-        
+    if (nums == null || nums.length == 0) return;        
+
+    int insertPos = 0;
+    for (int num: nums) {
+        if (num != 0) nums[insertPos++] = num;
+    }        
+
+    while (insertPos < nums.length) {
+        nums[insertPos++] = 0;
     }
 }
+}
+
