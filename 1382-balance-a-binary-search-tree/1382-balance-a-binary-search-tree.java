ed94ef5243320dd581145609797db7dfc5865699
Snehal Gavande <75498566+Snehal-gava@users.noreply.github.com>
2024-06-26 23:09:58 +0530

    Time: 2 ms (96.27%), Space: 46.4 MB (8.53%) - LeetHub

---
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode balanceBST(TreeNode root) {
+        List<Integer> inorder = new ArrayList<>();
+        inorderTraversal(root,inorder);
+        
+        return createBalancedBST(inorder,0,inorder.size()-1);
+    }
+    private void inorderTraversal(TreeNode root, List<Integer> inorder){
+        if(root==null) return;
+        
+        inorderTraversal(root.left,inorder);
+        inorder.add(root.val);
+        inorderTraversal(root.right,inorder);
+    }
+    private TreeNode createBalancedBST(
+        List<Integer> inorder,
+        int start,
+        int end
+    ) {
+        // Base case: if the start index is greater than the end index, return null
+        if (start > end) return null;
+
+        // Find the middle element of the current range
+        int mid = start + (end - start) / 2;
+
+        // Recursively construct the left and right subtrees
+        TreeNode leftSubtree = createBalancedBST(inorder, start, mid - 1);
+        TreeNode rightSubtree = createBalancedBST(inorder, mid + 1, end);
+
+        // Create a new node with the middle element and attach the subtrees
+        TreeNode node = new TreeNode(
+            inorder.get(mid),
+            leftSubtree,
+            rightSubtree
+        );
+        return node;
+    }
+}
